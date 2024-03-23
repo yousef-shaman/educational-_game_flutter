40e67753d426bc7cea74d442c85621734e7e5400
M1KRULS <aimaan403@gmail.com>
2024-03-24 00:10:00 +0300

    adding main pages files for both users

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:graduation_project_flutter/views/student/profile_page.dart';
-import 'package:graduation_project_flutter/views/student/home_page.dart';
-import 'package:graduation_project_flutter/views/student/leaderboard_page.dart';
+import 'package:graduation_project_flutter/views/faculty/main_page.dart';
+import 'package:graduation_project_flutter/views/student/main_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  bool isStudent = true;
+  runApp(MyApp(isStudent: isStudent));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key, required this.isStudent});
+  final bool isStudent;
 
   @override
   Widget build(BuildContext context) {
@@ -18,60 +19,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MainPage(),
+      home: isStudent ? const StudentMainPage() : const AdminMainPage(),
     );
   }
 }
-
-class MainPage extends StatefulWidget {
-  const MainPage({super.key});
-
-  @override
- State<MainPage> createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  int _currentIndex = 0;
-
-  final List<Widget> _screens = [
-    const ProfilePage(),
-    const HomePage(),
-     const Leaderboard(),
-    
-  ];
-
-  void _onTabTapped(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(toolbarHeight: 15,
-      ),
-      body: _screens[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: _onTabTapped,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person_rounded),
-            label: 'Profile',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.whatshot_rounded),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.leaderboard_rounded),
-            label: 'Leaderboard',
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-
